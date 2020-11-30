7a80b150e728ddf2003eed25ce394645f975a5a0
AndreasNg97 <54266373+AndreasNg97@users.noreply.github.com>
2020-11-30 14:47:38 +0100

    Updates

---
+++ precache-manifest.5170fc5c82a25b2cbefd21e6f6574cc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19b839667b26920e3d768677d050f42",
+    "url": "/react-hoved-prosjekt/index.html"
+  },
+  {
+    "revision": "1d8c3b41bb3e93cf5d13",
+    "url": "/react-hoved-prosjekt/static/css/main.0a981dfd.chunk.css"
+  },
+  {
+    "revision": "5b2727b12cfcc667ca72",
+    "url": "/react-hoved-prosjekt/static/js/2.40a3b6f8.chunk.js"
+  },
+  {
+    "revision": "d613d378e7cab204f1235d550f79265c",
+    "url": "/react-hoved-prosjekt/static/js/2.40a3b6f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8c3b41bb3e93cf5d13",
+    "url": "/react-hoved-prosjekt/static/js/main.dfbb184d.chunk.js"
+  },
+  {
+    "revision": "7aaba687ef36fa4b7b59",
+    "url": "/react-hoved-prosjekt/static/js/runtime-main.528b036d.js"
+  },
+  {
+    "revision": "15dc4169aac3b06263f19408b0bde4cc",
+    "url": "/react-hoved-prosjekt/static/media/body.15dc4169.png"
+  },
+  {
+    "revision": "3070066aaf19ea5ef7f12b17a1ec2a0a",
+    "url": "/react-hoved-prosjekt/static/media/poster.3070066a.jpg"
+  }
+]);
